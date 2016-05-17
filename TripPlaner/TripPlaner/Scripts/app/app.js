c272e5ad7ce031bce74fffd7c01c2081de9d16ba
Ilya Kozlov <i.love.trance.krsk@gmail.com>
2016-05-18 01:56:52 +0700

    placemarks

--- TripPlaner/TripPlaner/Scripts/app/app.js
+++ TripPlaner/TripPlaner/Scripts/app/app.js
@@ -51,10 +51,14 @@ var Application = function () {
     $scope.map = L.map('map');
 
     $scope.placemarkPanel = ko.observable(false);
+    $scope.activeMarker = ko.observable(null);
     $scope.activePlacemark = ko.observable({
+        id: 0,
         latitude: 0,
         longitude: 0
     });
+    $scope.uniqueId = 1;
+    $scope.placemarks = ko.observableArray([]);
 
     $scope.placemarkService = new AjaxODataHelper('/odata/Placemarks');
 
@@ -101,7 +105,7 @@ var Application = function () {
             var container = L.DomUtil.create('div'),
                 //startBtn = createButton('Start from this location', container),
                 //destBtn = createButton('Go to this location', container),
-                createBtn = createButton('create', container);
+                createBtn = createButton('create point here', container);
 
 
             //L.DomEvent.on(startBtn, 'click', function() {
@@ -118,6 +122,7 @@ var Application = function () {
             L.DomEvent.on(createBtn, 'click', function () {
                 $scope.placemarkPanel(true);
                 $scope.activePlacemark({
+                    id: 0,
                     latitude: e.latlng.lat,
                     longitude: e.latlng.lng
                 });
@@ -128,6 +133,56 @@ var Application = function () {
                 .setLatLng(e.latlng)
                 .openOn($scope.map);
         });
+    };
+
+    $scope.update = function () {
+        $scope.map.closePopup();
+
+        var p = $scope.activePlacemark();
+
+        if (p.id === 0) {
+            p.id = $scope.uniqueId++;
+
+            var marker = new L.Marker({
+                lat: p.latitude,
+                lng: p.longitude
+            }, {
+                id: p.id,
+                placemark: p,
+                isDirty: false,
+                draggable: true
+            });
+
+            marker.on('click', function (e) {
+                var marker = e.target;
+                console.log(marker);
+                $scope.activeMarker(marker);
+                $scope.activePlacemark(marker.options.placemark);
+                $scope.placemarkPanel(true);
+            });
+            marker.on('dragend', function (e) {
+                var marker = e.target;
+                var position = marker.getLatLng();
+                console.log(marker);
+                marker.options.placemark.latitude = position.lat;
+                marker.options.placemark.longitude = position.lng;
+                marker.options.isDirty = true;
+                marker._icon.style.backgroundColor = 'red';
+                marker._icon.style.borderRadius = '10px';
+                marker.bindPopup('Not saved on server').openPopup();
+
+                $scope.activeMarker(marker);
+                $scope.activePlacemark(marker.options.placemark);
+                $scope.placemarkPanel(true);
+            });
+            marker.addTo($scope.map);
+
+        } else {
+            var marker = $scope.activeMarker();
+            marker._icon.style.backgroundColor = 'transparent';
+        }
+
+        $scope.placemarkPanel(false);
     };
 
     return $scope;
